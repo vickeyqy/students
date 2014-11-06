ecee0d9960c3b6252750ee7f3b5c5879fcd3b093
vickeyqy <zhangran_198701@163.com>
2014-11-06 09:45:09 +0800

    yuanzu

--- 100jishuhe.py
+++ 100jishuhe.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-sum = 0
-n=99
-
-while n>0:
-    sum = sum+n
-    n=n-2
-print sum
+def sum():
+    sum = 0
+    n=99
+    while n>0:
+        sum = sum+n
+        n=n-2
+    print sum
+def bianli():
+    list = ['1','2','3','4']
+    list.append('5')
+    for x in list:
+        print x
+    trup = ('10','20',['30','40'])
+    for a in trup:
+        for b in trup(3):
+            print b
+bianbi()
