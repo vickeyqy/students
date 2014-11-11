b774a38d50f5f1b3357f05fcad389f6e4ac1f968
vickeyqy <zhangran_198701@163.com>
2014-11-11 17:04:45 +0800

    增加选择排序

--- paixu.py
+++ paixu.py
@@ -7,3 +7,21 @@ def bubble_sort (seq):
                 tmp = seq[j]
                 seq[j] = seq[i]
                 seq[i] =tmp
+
+def select_sort (seq):
+    for i  in range(len(seq)):
+        position = i
+        for j in range(i,len(seq)):
+            if seq[position] > seq[j]:
+                position = j
+        if position != i:
+            tmp = seq[position]
+            seq[position] = seq[i]
+            seq[i]= tmp
+
+
+seq1 = [30,15,25]
+select_sort(seq1)
+print seq1
+
+
