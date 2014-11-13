da798d2cc50b52eae232c331f101bbae8e778331
vickeyqy <zhangran_198701@163.com>
2014-11-13 16:37:50 +0800

    map的实现

--- hanshu.py
+++ hanshu.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def  f(x):
-    return x * x
-#L = map(f,[1,2,3,4,5,6,7])
-#print L
+def  f(x,y):
+    return x * y
 
-def mymap ()
+def fn(x):
+    return x*x
+
+def addfx():
+    L=[]
+    for i in range(1,10):
+        L.append(f(i))
+    print L
+#map 函数的实现
+#map（f，list []）
+def mymap(*args):
+    L = []
+    for i in args:
+        L.append(fn(i))
+    print L
+#递归
+def prod(*args):
+    print  reduce(f,args)
+#将数字变为list
+def listnum(num):
+    L =[]
+    for i in range(1,num):
+        L.append(i)
+    return L
+
+if  __name__ == "__main__":
+    list1=[1,2,3,4,5,6,7,8,9,10]
+    list2 = listnum(1000)
+    mymap(*list1)
+    prod(*list2)
+
+
+
